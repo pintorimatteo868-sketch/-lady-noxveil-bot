ede19aa820a96cf17b15c2535059f96b4e08a723
pintorimatteo868-sketch <324970787+pintorimatteo868-sketch@users.noreply.github.com>
2026-09-04 20:45:26 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -108,7 +108,6 @@ def main():
     app.add_handler(CommandHandler("help", help_command))
 app.add_handler(CommandHandler("help", help_command))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, auto_reply))
-
 print("Lady Noxveil bot is running...")
 app.run_polling()
     print("Lady Noxveil bot is running...")
